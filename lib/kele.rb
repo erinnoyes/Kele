eb44f13300faafec108e97d6d9c996fb6e729d25
Erin Noyes <emnoyes@gmail.com>
2018-05-11 16:11:56 -0700

    remove instance variables

--- lib/kele.rb
+++ lib/kele.rb
@@ -17,12 +17,12 @@ class Kele
 
   def get_me
     response = self.class.get(base_url("users/me"), headers: { "authorization" => @auth_token })
-    @user_data = JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
     response = self.class.get(base_url("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token })
-    @mentor_availability = JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 
 
